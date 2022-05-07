ac943e16a0c2959ed8a09b4e64b15d7430326a82
jasonf8080 <87998220+jasonf8080@users.noreply.github.com>
2022-05-06 20:47:26 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,127 +1,178 @@
 import { fetchData } from "./fetchData.js";
-import { displayItems, displayFixtures, displayNews } from "./displayItems.js";
-import { newsArticles } from "./news.js";
+import { setTrending } from "./trending.js";
+import { categoriesData } from "./utils.js";
+import { paginate } from "./trending.js";
+import { displayRecipe } from "./display.js";
+import { displayCategories } from "./display.js";
+import { selectRecipe } from "./category.js";
+import { hideLoader, showLoader } from "./loading.js";
+import { paginateAll } from "./sort.js";
 
 
 
-let baseURL = `https://www.thesportsdb.com/api/v1/json/2/lookuptable.php?l=4328&s=2021-2022`;
-let fixtureURL = 'https://www.thesportsdb.com/api/v1/json/50130162/eventspastleague.php?id=4328';
-const menuBtn = document.querySelector('.menu-icon');
-const menu = document.querySelector('.links');
-const links = document.querySelectorAll('.links li');
-const clubLink = document.querySelector('#club-link');
-const tableBody = document.querySelector('tbody');
-const latestResultsGrid = document.querySelector('.latest-results-grid');
-const teamsListPage = document.querySelector('.teams-list');
-const teamsList = document.querySelector('.teams-list ul');
+const links = document.querySelectorAll('.links a')
+const trendingSection = document.querySelector('.trending-grid');
+const categorySection = document.querySelector('.categories-grid');
+const numbers = document.querySelector('.pages-div');
+const numbersElements = [...document.querySelectorAll('.pages-div .number')];
+let trendingIndex = 0;
+let pages = [];
+const loader = document.querySelector('.loader-container');
+const searchBtn = document.querySelector('.search-btn');
+const exitBtn = document.querySelector('.exit-btn');
+const searchRecipeBtn = document.querySelector('.search-bar a');
+const searchModal = document.querySelector('.search-modal');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const menuBtn = document.querySelector('.menu');
+const menuExitBtn = document.querySelector('.side-menu .menu-exit');
+const sideMenu = document.querySelector('.side-menu');
+const allRecipesSection = document.querySelector('.all-recipes-grid');
+const allRecipeNumbers = document.querySelector('.numbers-div');
+let allRecipeIndex = 0;
 
 
 
 
-//EVENT LISTENERS
 
+//EVENT LISTENERS//
+const load = async() =>{
 
-const load = async () => {
-    displayNews(newsArticles);
-    displayData(baseURL);
-    adjustFixtures(fixtureURL);
+    document.title = 'Home';
+    const data = await fetchData();
+    const trending = setTrending(data);
+   
+   pages = paginate(trending);//whole array
+   displayRecipe(pages[trendingIndex], trendingSection);
+   const allPaginate = await paginateAll();
+    displayRecipe(allPaginate[allRecipeIndex], allRecipesSection)
+
+   displayCategories(categorySection, categoriesData);
+   
+   const categories = [...document.querySelectorAll('.category-item')];
+   selectCategory(categories);
+
+   const trendingRecipes = [...document.querySelectorAll('.recipe-item')];
+    selectRecipe(trendingRecipes);
 }
 
+
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
+    sideMenu.classList.add('active');
+})
+
+menuExitBtn.addEventListener('click', () => {
+    sideMenu.classList.remove('active');
 })
 
 links.forEach((link) => {
-    link.addEventListener('click', () => menu.classList.remove('active'))
+    link.addEventListener('click', () => {
+        sideMenu.classList.remove('active');
+    })
+})
+
+searchBtn.addEventListener('click', () => {
+    searchModal.classList.add('active');
+    window.addEventListener('scroll', disableScroll);
+    header.classList.add('blur');
+    nav.classList.add('blur');
+})
+
+
+exitBtn.addEventListener('click', () => {
+    searchModal.classList.remove('active');
+    window.removeEventListener('scroll', disableScroll);
+    header.classList.remove('blur');
+    nav.classList.remove('blur');
 })
 
 
 
-latestResultsGrid.addEventListener('click', (e) => {
-    //e.preventDefault();
-    const fixture = e.target.closest('a');
-    const fixtureID = fixture.getAttribute('data-id');
-    const fixtureImg = fixture.children[0].src;
+searchRecipeBtn.addEventListener('click', (e) => {
+    const inputValue = document.querySelector('.search-bar input').value;
+    const alert = document.querySelector('.alert');
 
-    const fixtureHome = fixture.children[1].children[0].textContent;
-    const fixtureHomeScore = fixture.children[1].children[1].textContent;
-    
-    const fixtureAway = fixture.children[2].children[0].textContent;
-    const fixtureAwayScore = fixture.children[2].children[1].textContent;
+    if(!inputValue){
+        e.preventDefault();
+        alert.style.display = 'block';
+        setTimeout(() => {
+            alert.style.display = 'none';
+        }, 1500)
+    } else {
+        localStorage.setItem('searchValue', inputValue);
+    }
+})
+
+
+
+
+numbers.addEventListener('click', async(e) => {
+    if(e.target.classList.contains('number')){
+       trendingIndex = parseInt(e.target.dataset.id - 1);
+       //await fetchData();
+       displayRecipe(pages[trendingIndex], trendingSection);
+       
+       const trendingRecipes = [...document.querySelectorAll('.recipe-item')];
+       selectRecipe(trendingRecipes);
+    }
+  })
+
+
+
+  numbersElements.forEach(number => {
+    number.addEventListener('click', () => {
+        numbersElements.forEach(number => {
+            number.classList.remove('active');
+        })
+        number.classList.add('active');
+    })
+}) 
+
+
+  allRecipeNumbers.addEventListener('click', async(e) => {
+    const allPaginate = await paginateAll();
+
+     if(e.target.classList.contains('number')){
+        allRecipeIndex  = parseInt(e.target.dataset.id - 1);
+        displayRecipe(allPaginate[allRecipeIndex], allRecipesSection);
+     }
+
+     const allRecipeNumbersEl = [...document.querySelectorAll('.numbers-div .number')];
+     allRecipeNumbersEl.forEach((number) => {
+        number.addEventListener('click', (number) => {
+            number.classList.remove('active');
+        })
+        
+     })
+
+     allRecipeNumbersEl[allRecipeIndex].classList.add('active');
+
+     const allRecipes = [...document.querySelectorAll('.recipe-item')];
+     selectRecipe(allRecipes);
+  })
+
+
+  
+
+
+
 
  
-    localStorage.setItem('fixtureID', fixtureID);
-    localStorage.setItem('fixtureImg', fixtureImg);
-    localStorage.setItem('fixtureHome', fixtureHome);
-    localStorage.setItem('fixtureHomeScore', fixtureHomeScore);
-    localStorage.setItem('fixtureAway', fixtureAway);
-    localStorage.setItem('fixtureAwayScore', fixtureAwayScore);
-})
-
-
-tableBody.addEventListener('click', (e) => {
-    const selectedID = e.target.closest('tr').getAttribute('data-id');
-    const teamID = e.target.closest('tr').getAttribute('id');// team name
-
-    localStorage.setItem('selectedID', selectedID)
-    localStorage.setItem('teamID', teamID);
-    
-    window.location = 'team.html';
-})
-
-
-//FUNCTIONS
-const displayData = async (url) =>{
-    const data = await fetchData(url);
-    const teams = data.table;
-    displayItems(teams);
-}
-
-const adjustFixtures = async (url) => {
-    const data = await fetchData(url);
-    const fixtures = data.events;
-    displayFixtures(fixtures, latestResultsGrid);
-}
-
-
-
-//Reset Table
-const matchResult = window.matchMedia("(max-width: 768px)");
-
-function isSmallScreen(){
-    if(window.matchMedia("(max-width: 768px)").matches){
-        const positionEl = document.querySelector('.position');
-        const playedEl = document.querySelector('.played');
-        const pointsEl = document.querySelector('.points');
-
-        positionEl.textContent = 'POS'
-        playedEl.textContent = 'PL'
-        pointsEl.textContent = 'PTS'
-        resetLetter();
-
-    }
-}
-
-function resetLetter(){
-    const winRecord = [...document.querySelectorAll('.wld')];
-        winRecord.forEach((item) => {
-        let itemContent = item.textContent;
-        const letter = itemContent.charAt(0);
-
-        item.textContent = letter;
+const selectCategory = (categories) => {
+    categories.forEach((category) => {
+        category.addEventListener('click', (e) => {
+           let selectedCategory = e.currentTarget.dataset.id;
+            localStorage.setItem('category', selectedCategory);
+        })
     })
 }
 
-isSmallScreen();
+
+//FUNCTIONS 
+
+function disableScroll(){
+    window.scrollTo(0, 0);
+}
 
 
-window.addEventListener('DOMContentLoaded', load);
-
-
-
-
-
-
-
-
-
+window.addEventListener('load', load);
